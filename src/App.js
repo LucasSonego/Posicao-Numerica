a87a75d17718d019c64e54c57e06839157f10cdc
Lucas Sônego <sonego_lucas@outlook.com>
2020-11-06 00:55:47 -0300

    :sparkles: feat: Estados para os slots e os números soltos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
+  const DEFAULT_SLOT_AMOUNT = 50;
+
+  const [slots, setSlots] = useState([]);
+  const [spareNumbers, setSpareNumbers] = useState([1, 2, 3, 4, 5]);
+  const [showNumbers, setShowNumbers] = useState(false);
+
+  useEffect(() => {
+    let defaultSlots = [];
+    for (let index = 0; index < DEFAULT_SLOT_AMOUNT; index++) {
+      defaultSlots.push(null);
+    }
+    setSlots(defaultSlots);
+  }, []);
+
   return <div className="App"></div>;
 }
 
