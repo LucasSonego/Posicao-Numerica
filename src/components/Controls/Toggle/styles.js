534867d24558586b05d1499a84add906844f38d9
Lucas Sônego <sonego_lucas@outlook.com>
2020-11-13 17:04:31 -0300

    :sparkles: feat: Componente toggle que será utilizado no painel de controles

---
+++ src/components/Controls/Toggle/styles.js
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  cursor: pointer;
+  background-color: #dcdde1;
+  height: 28px;
+  width: 48px;
+  border-radius: 20px;
+  padding: 4px;
+  box-sizing: border-box;
+  transition: 0.3s ease-in-out;
+  .ball {
+    height: 20px;
+    width: 20px;
+    border-radius: 10px;
+    background-color: white;
+    transition: 0.3s ease-out;
+  }
+  ${props =>
+    props.value
+      ? css`
+          .ball {
+            margin-left: 19px;
+          }
+          background-color: #00a8ff;
+        `
+      : css`
+          .ball {
+            margin-left: 0px;
+          }
+          background-color: #dedde1;
+        `}
+`;
